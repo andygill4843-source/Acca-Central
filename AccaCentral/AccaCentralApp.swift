b3de3dba66612ba333990c4e941bb0ad57d77d04
Andy Gill <andygill@macbookpro.home>
2026-08-04 22:46:47 +0100

    Initial commit: models, services, auth, core screens, Firestore rules

---
+++ AccaCentral/AccaCentralApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccaCentralApp.swift
+//  AccaCentral
+//
+//  Created by Andy Gill on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct AccaCentralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccaCentralTests/AccaCentralTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccaCentralTests.swift
+//  AccaCentralTests
+//
+//  Created by Andy Gill on 04/08/2026.
+//
+
+import Testing
+@testable import AccaCentral
+
+struct AccaCentralTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
